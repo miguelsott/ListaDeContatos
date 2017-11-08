705e36f43511cba239f3969ee95509ab383ed560
Serra <lucasserra03@gmail.com>
2017-11-08 08:38:45 -0200

    adicionando botão, campo de texto

---
+++ app/src/main/java/com/example/dwarfmedown/listadecontatos/add_contact/AddContactActivity.java
@@ -0,0 +1,14 @@
+package com.example.dwarfmedown.listadecontatos.add_contact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.dwarfmedown.listadecontatos.R;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
